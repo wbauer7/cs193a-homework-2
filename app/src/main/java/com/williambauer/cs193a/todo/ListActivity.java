46dc95ce531d89dfd8b4866975ee3d7144154a10
william <github@williambauer.com>
2016-01-25 21:54:53 -0800

    List now saved onStop() and restored onStart().

--- app/src/main/java/com/williambauer/cs193a/todo/ListActivity.java
+++ app/src/main/java/com/williambauer/cs193a/todo/ListActivity.java
@@ -2,15 +2,21 @@ package com.williambauer.cs193a.todo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ListActivity extends AppCompatActivity {
+    private static final String LIST_FILENAME = "listFile.txt";
+
     ArrayList<String> listArr;
     ArrayAdapter<String> listAdapter;
 
@@ -18,8 +24,6 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-
-        initList();
 
         // add a longClick listener to remove items
         ListView listView = (ListView) findViewById(R.id.listView);
@@ -36,11 +40,24 @@ public class ListActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        initList();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        saveListToFile();
+    }
+
     // initializes the list (internally and in GUI)
     private void initList() {
 
         if (listArr == null) {
             listArr = new ArrayList<>();
+            readListFromFile();
         }
 
         listAdapter = new ArrayAdapter<>(
@@ -59,6 +76,52 @@ public class ListActivity extends AppCompatActivity {
         listAdapter.notifyDataSetChanged();
     }
 
+    // saves the contents of the list to persistent storage
+    // Adapted from assignment spec and Marty's lecture notes
+    private void saveListToFile() {
+        try {
+            PrintStream out = new PrintStream(
+                    openFileOutput(LIST_FILENAME, MODE_PRIVATE)
+            );
+
+            for (int i = 0; i < listArr.size(); i++) {
+                out.println(listArr.get(i));
+            }
+
+            out.close();
+
+        } catch (IOException ioe) {
+            Log.e("saveListToFile", ioe.toString());
+        }
+    }
+
+    // reads in a list from persistent storage
+    // does not preserve internal list
+    // Adapted from assignment spec and Marty's lecture notes
+    private void readListFromFile() {
+        // clear existing list
+        if (listArr == null || listArr.size() > 0) {
+            listArr = new ArrayList<>();
+        }
+
+        try {
+            Scanner scan = new Scanner(openFileInput(LIST_FILENAME));
+
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                listArr.add(line);
+            }
+
+            if (listAdapter != null) {
+                listAdapter.notifyDataSetChanged();
+            }
+
+        } catch (IOException ioe) {
+            Log.e("ReadListFromFile", ioe.toString());
+        }
+
+    }
+
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putStringArrayList("listArr", listArr);
@@ -69,4 +132,5 @@ public class ListActivity extends AppCompatActivity {
         listArr = inState.getStringArrayList("listArr");
         initList();
     }
+
 }
